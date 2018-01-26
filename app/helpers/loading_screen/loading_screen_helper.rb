934419fe100daf5fa48a1e08c66d20bc6e653271
Swakhar <swakhar.me@gmail.com>
2018-01-26 14:13:24 +0600

    created new options for animation

--- app/helpers/loading_screen/loading_screen_helper.rb
+++ app/helpers/loading_screen/loading_screen_helper.rb
@@ -3,8 +3,17 @@ module LoadingScreen::LoadingScreenHelper
   def loading_screen(options={})
     content_tag :div, '', class: 'loading_screen-spinner' do
       if options[:gif].present?
-        content_tag :div do
+        content_tag :div, '', class: 'loading_screen-gif' do
           image_tag(options[:gif], id: 'loading_screen-spinner-image')
+        end
+      elsif options[:class].present?
+        if options[:class] == 'double_bounce'
+          [].tap do |array|
+            array << content_tag(:div, '', class: 'double_bounce1')
+            array << content_tag(:div, '', class: 'double_bounce2')
+          end.join.html_safe
+        elsif options[:class] == 'loading_screen-spinner-css'
+          content_tag :div, '', id: 'loading_screen-spinner-css'
         end
       else
         content_tag :div, '', id: 'loading_screen-spinner-css'
