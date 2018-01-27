3aa6c9d3d4805484b0ab5b4f14b70fcc3d8f544e
Mujadded Al Rabbani Alif <Mujadded.alif@gmail.com>
2018-01-27 23:37:40 +0600

    Error handle, js change

--- app/helpers/loading_screen/loading_screen_helper.rb
+++ app/helpers/loading_screen/loading_screen_helper.rb
@@ -2,23 +2,52 @@ module LoadingScreen::LoadingScreenHelper
 
   def loading_screen(options={})
     options[:style] = :default unless options[:style].present?
+
     content_tag :div, '', class: 'loading_screen-spinner' do
       if options[:gif].present?
-        content_tag :div, '', class: 'loading_screen-gif' do
-          image_tag(options[:gif])
-        end
+        gif_handler options
       else
-        content_tag :div, '', class: 'loading_screen-css' do
-          if options[:style] == :double_bounce
-            [].tap do |array|
-              array << content_tag(:div, '', id: 'loading_screen-double_bounce1')
-              array << content_tag(:div, '', id: 'loading_screen-double_bounce2')
-            end.join.html_safe
-          else
-            content_tag :div, '', id: 'loading_screen-rotating-square'
-          end
-        end
+        css_handler options
       end
+    end
+  rescue NameError => e
+    error_handler
+  end
+
+  def gif_handler(options)
+    content_tag :div, '', class: 'loading_screen-gif' do
+      image_tag(options[:gif])
+    end
+  rescue AssetNotFound => e
+    raise Exception.NameError
+  end
+
+  def css_handler(options)
+    content_tag :div, '', class: 'loading_screen-css' do
+      if options[:style] == :double_bounce
+        load_double_bounce
+      elsif options[:style] == :default
+        load_rotate_square
+      else
+        raise Exception.NameError
+      end
+    end
+  end
+
+  def load_double_bounce
+    [].tap do |array|
+      array << content_tag(:div, '', id: 'loading_screen-double_bounce1')
+      array << content_tag(:div, '', id: 'loading_screen-double_bounce2')
+    end.join.html_safe
+  end
+
+  def load_rotate_square
+    content_tag :div, '', id: 'loading_screen-rotating-square'
+  end
+
+  def error_handler
+    content_tag :div, '', class: 'loading_screen-spinner' do
+      content_tag :div, 'Something went wrong!', class: 'loading_screen-error'
     end
   end
 end
--- vendor/assets/javascripts/loading_screen.js
+++ vendor/assets/javascripts/loading_screen.js
@@ -1,16 +1,24 @@
-$( window ).bind('load',function() {
-
-    // hide spinner
-    $(".loading_screen-spinner").fadeOut();
-
-    // show spinner on AJAX start
-    $(document).ajaxStart(function(){
-        $(".loading_screen-spinner").show();
+$(document).ready(function(){
+    $(document).keyup(function(e) {
+        if (e.keyCode == 27) { // escape key maps to keycode `27`
+            $(".loading_screen-spinner").fadeOut();
+        }
     });
 
-    // hide spinner on AJAX stop
-    $(document).ajaxStop(function(){
+    $( window ).bind('load',function() {
+
+        // hide spinner
         $(".loading_screen-spinner").fadeOut();
-    });
 
+        // show spinner on AJAX start
+        $(document).ajaxStart(function(){
+            $(".loading_screen-spinner").show();
+        });
+
+        // hide spinner on AJAX stop
+        $(document).ajaxStop(function(){
+            $(".loading_screen-spinner").fadeOut();
+        });
+
+    });
 });
